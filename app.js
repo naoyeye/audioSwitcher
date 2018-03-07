b4abbabac8d2c278138eb572da2a42da43417693
hanjiyun <hanjiyun@douban.com>
2018-03-08 03:15:13 +0800

    HttpsRedirect

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var compression = require('compression'); // 开启 gzip 压缩
+
 var AV = require('leanengine');
 
 // 加载云函数定义，你可以将云函数拆分到多个文件方便管理，但需要在主文件中加载它们
@@ -31,12 +32,14 @@ app.use(timeout('15s'));
 app.use(AV.express());
 
 app.enable('trust proxy');
+
 // 需要重定向到 HTTPS 可去除下一行的注释。
-// app.use(AV.Cloud.HttpsRedirect());
+app.use(AV.Cloud.HttpsRedirect());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 
 app.get('/', function(req, res) {
   res.render('index', {
