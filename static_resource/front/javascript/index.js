b906eb391605227861448ca868e65ebec2c1d637
hanjiyun <hanjiyun@douban.com>
2018-03-08 13:18:27 +0800

    add audio controller

--- static_resource/front/javascript/index.js
+++ static_resource/front/javascript/index.js
@@ -39,14 +39,37 @@ $(function() {
   let dragging = false
 
   let wrapEle = document.getElementById('handler-wrap')
-  let leftEle = document.getElementById('left')
-  let rightEle = document.getElementById('right')
+  // let leftEle = document.getElementById('left')
+  // let rightEle = document.getElementById('right')
 
   // console.log(leftEle, defaultWidth)
-  document.getElementById("left").innerHTML = defaultWidth
-  document.getElementById("right").innerHTML = defaultWidth
+  // document.getElementById("left").innerHTML = defaultWidth
+  // document.getElementById("right").innerHTML = defaultWidth
 
 
+  var leftAudio = document.getElementById('left-audio');
+  var rightAudio = document.getElementById('right-audio');
+  var $controlBtn = $('#stop-play-btn');
+
+  leftAudio.loop = true;
+  rightAudio.loop = true;
+
+  var AudioContext = window.AudioContext || window.webkitAudioContext;
+  var context = new AudioContext();
+  var leftAudioSource = context.createMediaElementSource(leftAudio);
+  var rightAudioSource = context.createMediaElementSource(rightAudio);
+
+  var leftGain = context.createGain();
+  var rightGain = context.createGain();
+
+  leftAudioSource.connect(leftGain);
+  rightAudioSource.connect(rightGain);
+
+  leftGain.connect(context.destination);
+  rightGain.connect(context.destination);
+
+  leftAudio.play();
+  rightAudio.play();
 
   function _drag_init(elem) {
     selected = elem;
@@ -67,24 +90,30 @@ $(function() {
 
       if (selected !== null) {
         wrapEle.style.width = x_pos + 'px';
-        leftEle.innerText = x_pos
-        rightEle.innerText = winW - x_pos
+        // leftEle.innerText = x_pos
+        // rightEle.innerText = winW - x_pos
+
+        let leftVolume = ((x_pos / winW)).toFixed(2)
+        let rightVolume = 1 - leftVolume
+
+        leftGain.gain.setValueAtTime(leftVolume, context.currentTime)
+        rightGain.gain.setValueAtTime(rightVolume, context.currentTime)
       }
   }
 
 
   function _destroy(e) {
-    x_pos = (document.all ? window.event.clientX : e.pageX) - x_elem;
-    if (x_pos < 0 || x_pos === 10) {
-      x_pos = 10
-    }
+    // x_pos = (document.all ? window.event.clientX : e.pageX) - x_elem;
+    // if (x_pos < 0 || x_pos === 10) {
+    //   x_pos = 10
+    // }
 
-    if (x_pos === winW || x_pos > winW) {
-      x_pos = winW - 10
-    }
-    if (dragging) {
-      console.log(x_pos)
-    }
+    // if (x_pos === winW || x_pos > winW) {
+    //   x_pos = winW - 10
+    // }
+    // if (dragging) {
+      // console.log(x_pos)
+    // }
 
     dragging = false;
     selected = null;
@@ -99,6 +128,19 @@ $(function() {
   document.onmousemove = _move_elem;
   document.onmouseup = _destroy;
 
-
+  let playing = true;
+  $controlBtn.click((e) => {
+    if (playing) {
+      leftAudio.pause();
+      rightAudio.pause();
+      playing = false;
+      $controlBtn.removeClass('playing');
+    } else {
+      leftAudio.play();
+      rightAudio.play();
+      playing = true;
+      $controlBtn.addClass('playing');
+    }
+  })
 
 })
